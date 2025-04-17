de3e42453a01e1cf5e93833f1203da9055812677
Adam7820 <adamouzgane@gmail.com>
2025-04-17 22:54:48 +0200

    ajout du fichier database qui permet de connecter la bdd au projet

---
+++ database.php
@@ -0,0 +1,23 @@
+<?php
+function connectToDbAndGetPdo() {
+    $host = "localhost";
+    $dbname = "Projet_PG2";
+    $user = "root";
+    $password = "";
+    
+    try {
+        $db = new PDO(
+            "mysql:host=$host;dbname=$dbname",
+            $user,
+            $password,
+            array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8", PDO::MYSQL_ATTR_DIRECT_QUERY => true)
+        );
+        return $db;
+    } catch (PDOException $e) {
+        echo "Erreur de connexion : " . $e->getMessage();
+        return null;
+    }
+}
+
+
+?>
